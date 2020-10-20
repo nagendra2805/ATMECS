dd5821e2e6a01a7be23492c89137e8b93d48f17b
nagendra2805 <45525384+nagendra2805@users.noreply.github.com>
2020-10-20 21:58:37 +0530

    Add files via upload

---
+++ ContactsMangementSystem/Interfaces/IContactService.cs
@@ -0,0 +1,12 @@
+﻿using ContactsMangementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsMangementSystem.Interfaces
+{
+    public interface IContactService
+    {
+        List<Contact> CallContatcAPI(string requestUrl, Contact contact);
+    }
+}
