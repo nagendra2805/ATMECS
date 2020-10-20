23d0c8402d8742c5d8474be88d5264bb3f715f8b
Nagendra Duvvapu <nagendra.d522@gmail.com>
2020-10-20 17:11:38 +0530

    Final commit

---
+++ ContactsWebAPI/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsWebAPI.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public List<string> MobileNumbers { get; set; }
+        public List<string> EmailIds { get; set; }
+    }
+}
---
+++ UnitTests/ContactServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using ContactsWebAPI.Controllers;
+using ContactsWebAPI.Interfaces;
+using ContactsWebAPI.Models;
+using ContactsWebAPI.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ContactServiceTests
+    {
+        [Fact]
+        public void ShouldReturnListofContacts()
+        {
+            var contact = new Contact()
+            {
+                FirstName = "dummynfame",
+                LastName = "dummylname",
+                DateofBirth = Convert.ToDateTime("05/05/2020"),
+                MobileNumbers = new List<string>() { "123456789" },
+                EmailIds = new List<string>() { "some@gmail.com" },
+            };
+
+            var service = new ServiceCollection();
+            service.AddTransient<IContactService, ContactService>();
+            var serviceProvider = service.BuildServiceProvider();
+
+            var contactservice = serviceProvider.GetRequiredService<IContactService>();
+
+            var response = contactservice.CreateContact(contact);
+            response.Should().NotBeNull();
+            response.Should().BeOfType<List<Contact>>();
+        }
+    }
+}
