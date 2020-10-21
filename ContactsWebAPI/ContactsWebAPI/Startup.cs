e593282d67760449faa0920f76fbee3613771fa4
nagendra2805 <45525384+nagendra2805@users.noreply.github.com>
2020-10-21 09:57:25 +0530

    Update Startup.cs

--- ContactsWebAPI/ContactsWebAPI/Startup.cs
+++ ContactsWebAPI/ContactsWebAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace ContactsWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddTransient<IContactService, ContactService>();
+            services.AddSingleton<IContactService, ContactService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
